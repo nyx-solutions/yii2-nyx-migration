291748a9c688eca8542e15d0b731cf6d369d8af1
Jonatas Sas <atendimento@jsas.com.br>
2016-02-13 15:36:49 -0200

    Migration

--- Migration.php
+++ Migration.php
@@ -12,22 +12,51 @@
      */
     class Migration extends \yii\db\Migration
     {
+        #region Constants
         const YES             = 1;
         const NO              = 0;
 
         const STATUS_ACTIVE   = 1;
         const STATUS_INACTIVE = 0;
+        #endregion
+
+        /**
+         * @var bool
+         */
+        protected $onlyMySql = false;
 
         /**
          * @var string
          */
-        public $tableName;
+        protected $tableName;
 
         /**
          * @var string
          */
-        public $tableOptions;
+        protected $tableOptions;
 
+        #region Initialization
+        /**
+         * @inheritdoc
+         *
+         * @throws NotSupportedException
+         */
+        public function init()
+        {
+            parent::init();
+
+            if ($this->db->driverName === 'mysql') {
+                $this->tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            } else {
+                if ((bool)$this->onlyMySql) {
+                    throw new NotSupportedException('MySQL required.');
+                }
+            }
+        }
+        #endregion
+
+        #region DataBase
+        #region DataBase Tables
         /**
          * @param string $table
          *
@@ -76,7 +105,9 @@
                 return false;
             }
         }
+        #endregion
 
+        #region DataBase Views
         /**
          * @param string $view
          *
@@ -105,4 +136,41 @@
                 $this->execute("DROP VIEW {$view}");
             }
         }
+        #endregion
+
+        #region DataBase Table Names
+        /**
+         * @param string $name
+         *
+         * @return string
+         */
+        public function getTableName($name = '')
+        {
+            return '{{%'.((!empty($name)) ? $name : $this->getSimpleTableName()).'}}';
+        }
+
+        /**
+         * @param string $name
+         *
+         * @return string
+         */
+        public function withTableName($name)
+        {
+            return '{{%'.$this->getSimpleTableName().'_'.$name.'}}';
+        }
+
+        /**
+         * @return string
+         */
+        protected function getSimpleTableName()
+        {
+            $tableName = (string)$this->tableName;
+
+            $tableName = preg_replace('/^\{\{%/', '', $tableName);
+            $tableName = preg_replace('/\}\}$/', '', $tableName);
+
+            return $tableName;
+        }
+        #endregion
+        #endregion
     }
