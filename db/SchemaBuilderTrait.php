a45a5eca8d421b799a4cefc339e26561b0f7e7c2
Jonatas Sas <atendimento@jsas.com.br>
2016-07-08 10:16:50 -0300

    1.1.2

--- db/SchemaBuilderTrait.php
+++ db/SchemaBuilderTrait.php
@@ -15,6 +15,38 @@
         use \yii\db\SchemaBuilderTrait;
 
         /**
+         * @return ColumnSchemaBuilder the column instance which can be further customized.
+         */
+        public function tinyText()
+        {
+            return $this->string(255);
+        }
+
+        /**
+         * @return ColumnSchemaBuilder the column instance which can be further customized.
+         */
+        public function mediumText()
+        {
+            if ($this->getDb()->driverName == 'mysql') {
+                return $this->getDb()->getSchema()->createColumnSchemaBuilder('mediumtext');
+            }
+
+            return $this->text();
+        }
+
+        /**
+         * @return ColumnSchemaBuilder the column instance which can be further customized.
+         */
+        public function longText()
+        {
+            if ($this->getDb()->driverName == 'mysql') {
+                return $this->getDb()->getSchema()->createColumnSchemaBuilder('longtext');
+            }
+
+            return $this->text();
+        }
+
+        /**
          * @param bool $true
          *
          * @return ColumnSchemaBuilder the column instance which can be further customized.
